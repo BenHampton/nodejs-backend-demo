a2d79909d8c431beb933632debbf5c96b4d213b7
Ben Hampton <BenHampton11@yahoo.com>
2026-06-05 12:44:03 -0500

    touch ups

--- nodejs-demo-db-script.sql
+++ nodejs-demo-db-script.sql
@@ -1,9 +1,11 @@
 select current_database();
 
 select * from _prisma_migrations;
+select * from refresh_tokens;
+
+
+select * from users;
 
 select * from posts;
 
-select * from refresh_tokens;
 
-select * from users;
